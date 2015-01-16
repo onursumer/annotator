b6bc1957091399c2af73edb4978d1aa2cfbea78e
Onur Sumer <s.onur.sumer@gmail.com>
2015-01-16 17:05:11 -0500

    moved config parameters into a properties file

---
+++ src/main/Config.java
@@ -0,0 +1,60 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.mskcc.cbio.annotator.AnnotatorConfig;
+
+public class Config
+{
+	String DEFAULT_PROPERTIES_FILE = "annotator.properties";
+	
+	public AnnotatorConfig loadConfig(String configFilename) throws IOException
+	{
+		Properties props = loadProperties(getResourceStream(configFilename));
+
+		AnnotatorConfig config = new AnnotatorConfig();
+
+		config.setMaf2maf(props.getProperty("annotator.maf2maf"));
+		config.setVcf2maf(props.getProperty("annotator.vcf2maf"));
+		config.setVepPath(props.getProperty("annotator.vepPath"));
+		config.setVepData(props.getProperty("annotator.vepData"));
+		config.setRefFasta(props.getProperty("annotator.refFasta"));
+		config.setExcludeCols(props.getProperty("annotator.excludeCols"));
+		config.setIntermediateMaf(props.getProperty("annotator.intermediateMaf"));
+
+		return config;
+	}
+
+	public AnnotatorConfig loadConfig() throws IOException
+	{
+		return loadConfig(DEFAULT_PROPERTIES_FILE);
+	}
+
+	private static Properties loadProperties(InputStream resourceInputStream) throws IOException
+    {
+        Properties properties = new Properties();
+
+        properties.load(resourceInputStream);
+        resourceInputStream.close();
+
+        return properties;
+    }
+	
+    private static InputStream getResourceStream(String resourceFilename)
+    {
+    	InputStream resourceFIS = null;
+
+    	try {
+    		resourceFIS = new FileInputStream(resourceFilename);
+    	}
+    	catch (FileNotFoundException e) {            
+    		// Failed to read properties file: resourceFilename
+    	}
+ 
+    	return resourceFIS;
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,46 +6,16 @@ import org.mskcc.cbio.annotator.MultiFileMaf2Maf;
 
 public class Main
 {
-	public static AnnotatorConfig localConfig()
+	public static void main(String[] args) throws IOException
 	{
-		AnnotatorConfig config = new AnnotatorConfig();
-		
-		config.setMaf2maf("/home/sos/MSKCC/portal/vcf2maf/maf2maf.pl");
-		config.setVcf2maf("/home/sos/MSKCC/portal/vcf2maf/vcf2maf.pl");
-		config.setVepPath("/home/sos/MSKCC/portal/vep");
-		config.setVepData("/home/sos/MSKCC/portal/vep");
-		config.setRefFasta("/home/sos/MSKCC/portal/vep/homo_sapiens/76_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa");
-		config.setExcludeCols("oncotator");
-		config.setIntermediateMaf("/home/sos/MSKCC/portal/annotator/annotator_out.maf");
-		
-		return config;
-	}
-	
-	public static AnnotatorConfig remoteConfig()
-	{
-		AnnotatorConfig config = new AnnotatorConfig();
-		
-		config.setMaf2maf("/ssd-data/cmo/opt/vcf2maf/maf2maf.pl");
-		config.setVcf2maf("/ssd-data/cmo/opt/vcf2maf/vcf2maf.pl");
-		config.setVepPath("/ssd-data/cmo/opt/vep");
-		config.setVepData("/ssd-data/cmo/opt/vep");
-		config.setRefFasta("/ssd-data/cmo/srv/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa");
-		config.setExcludeCols("oncotator");
-		config.setIntermediateMaf("/data/onur/annotator/annotator_out.maf");
-		
-		return config;
-	}
-	
-	public static void main(String[] args)
-	{
-		//AnnotatorConfig config = localConfig();
-		AnnotatorConfig config = remoteConfig();
-		MultiFileMaf2Maf annotator = new MultiFileMaf2Maf(config);
-		
-		// TODO portal-data directory... 
+		// TODO check for config file!
+		Config config = new Config();
+		AnnotatorConfig annoConfig = config.loadConfig();
+		MultiFileMaf2Maf annotator = new MultiFileMaf2Maf(annoConfig);
+
+		// TODO move these into properties?
+		// default source and target directories...
 		String sourceDir = "/home/sos/CS/idea_worksapce/portal-data/";
-		
-		// TODO somewhere out in space...
 		String targetDir = "/home/sos/MSKCC/portal/data/maf2maf/";
 		
 		if (args.length > 0)
