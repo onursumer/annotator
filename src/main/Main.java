8917f7709b2202bc27f0be6cc6e0bd6f13d970a9
Onur Sumer <s.onur.sumer@gmail.com>
2015-03-30 20:31:04 -0400

    added validation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,10 @@
 package main;
 import java.io.IOException;
+import java.util.Date;
 
 import org.mskcc.cbio.annotator.AnnotatorConfig;
 import org.mskcc.cbio.annotator.MultiFileMaf2Maf;
+import org.mskcc.cbio.annotator.MultiFileValidator;
 
 public class Main
 {
@@ -12,12 +14,14 @@ public class Main
 		Config config = new Config();
 		AnnotatorConfig annoConfig = config.loadConfig();
 		MultiFileMaf2Maf annotator = new MultiFileMaf2Maf(annoConfig);
+		MultiFileValidator validator = new MultiFileValidator();
 
 		// TODO move these into properties?
 		// default source and target directories...
 		String sourceDir = "/home/sos/CS/idea_worksapce/portal-data/";
 		String targetDir = "/home/sos/MSKCC/portal/data/maf2maf/";
-		
+		String mode = "--annotate";
+
 		if (args.length > 0)
 		{
 			sourceDir = args[0];
@@ -27,11 +31,33 @@ public class Main
 		{
 			targetDir = args[1];
 		}
-		
-		try {
-			annotator.annotate(sourceDir, targetDir);
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		if (args.length > 2)
+		{
+			mode = args[2];
+		}
+
+		if (mode.equalsIgnoreCase("--validate-only"))
+		{
+			System.out.println("[" + new Date() + "] Started validating output...");
+			validator.annotate(sourceDir, targetDir);
+		}
+		else
+		{
+			try
+			{
+				annotator.annotate(sourceDir, targetDir);
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+
+			if (mode.equalsIgnoreCase("--validate"))
+			{
+				System.out.println("[" + new Date() + "] Started validating output...");
+				validator.annotate(sourceDir, targetDir);
+			}
 		}
 	}
 }
