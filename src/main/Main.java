7150ac4f594a7c816795e35ce18ebe836a34f4fc
Onur Sumer <s.onur.sumer@gmail.com>
2015-04-03 20:15:58 -0400

    added sanitize option

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.mskcc.cbio.annotator.AnnotatorConfig;
 import org.mskcc.cbio.annotator.MultiFileMaf2Maf;
 import org.mskcc.cbio.annotator.MultiFileValidator;
+import org.mskcc.cbio.annotator.MultiFileSanitizer;
 
 public class Main
 {
@@ -15,6 +16,7 @@ public class Main
 		AnnotatorConfig annoConfig = config.loadConfig();
 		MultiFileMaf2Maf annotator = new MultiFileMaf2Maf(annoConfig);
 		MultiFileValidator validator = new MultiFileValidator();
+		MultiFileSanitizer sanitizer = new MultiFileSanitizer();
 
 		// TODO move these into properties?
 		// default source and target directories...
@@ -41,6 +43,11 @@ public class Main
 		{
 			System.out.println("[" + new Date() + "] Started validating output...");
 			validator.annotate(sourceDir, targetDir);
+		}
+		else if (mode.equalsIgnoreCase("--sanitize-only"))
+		{
+			System.out.println("[" + new Date() + "] Started sanitizing input...");
+			sanitizer.annotate(sourceDir, targetDir);
 		}
 		else
 		{
